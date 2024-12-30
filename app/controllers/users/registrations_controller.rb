8db9bab0bad2234758d99eae6f141fefca5a52f5
EvAyano <g.ayanohisamoto@gmail.com>
2024-12-30 17:27:05 +0900

    fix(message): Error message for account.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,9 +12,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
     @user = current_user
 
     if @user.update(email_params)
-      redirect_to email_update_success_path
+      render :email_update_success
     else
-      render 'devise/registrations/edit_email'
+      flash[:alert] = @user.errors.full_messages
+      redirect_to edit_user_email_path
     end
   end
 
@@ -26,35 +27,34 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def update_password
-  
     @user = current_user
-  
+
     if params[:user][:current_password].blank?
       @user.errors.add(:current_password, "を入力してください")
     end
-  
+
     if params[:user][:password].blank?
       @user.errors.add(:password, "を入力してください")
     end
-  
+
     if params[:user][:password_confirmation].blank?
       @user.errors.add(:password_confirmation, "を入力してください")
     end
-  
+
     if @user.errors.any?
-      render :edit_password and return
+      flash[:alert] = @user.errors.full_messages
+      redirect_to edit_custom_user_password_path and return
     end
-  
+    
+
     if @user.update_with_password(password_params)
       bypass_sign_in(@user)
-      redirect_to password_update_success_path, notice: 'パスワードが更新されました。'
+      redirect_to password_update_success_path
     else
-      render :edit_password
+      flash[:alert] = @user.errors.full_messages.join(", ")
+      redirect_to edit_custom_user_password_path and return
     end
   end
-      
-  
-  
 
   def edit_nickname
     @user = current_user
@@ -63,10 +63,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def update_nickname
     @user = current_user
 
+    if params[:user][:nickname].blank?
+      @user.errors.add(:nickname, "を入力してください")
+    end
+
+    if @user.errors.any?
+      flash[:alert] = @user.errors.full_messages
+      redirect_to edit_user_nickname_path and return
+    end
+
     if @user.update(nickname_params)
-      redirect_to edit_user_registration_path, notice: 'ニックネームが更新されました。'
-    else
-      render :edit_nickname
+      redirect_to edit_user_registration_path
     end
   end
 
@@ -79,11 +86,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
     @user = current_user
 
     if @user.update(user_avatar_params)
-      flash[:notice] = 'プロフィール画像を更新しました。'
       redirect_to edit_user_registration_path
     else
-      flash.now[:alert] = 'プロフィール画像の更新に失敗しました。'
-      render 'devise/registrations/edit_avatar'
+      redirect_to edit_user_avatar_path and return
     end
   end
 
@@ -105,40 +110,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
     params.require(:user).permit(:avatar)
   end
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
-
-  # POST /resource
-  # def create
-  #   super
-  # end
-
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource
-  # def update
-  #   super
-  # end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
   protected
 
   def email_params
@@ -149,24 +120,4 @@ class Users::RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :password_confirmation])
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :email, :password, :password_confirmation, :current_password])
   end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
 end
