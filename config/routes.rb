5519943125c019452363e31083ecc42876aa4d71
EvAyano <g.ayanohisamoto@gmail.com>
2025-01-08 01:26:11 +0900

    added 500 error custom view

--- config/routes.rb
+++ config/routes.rb
@@ -46,5 +46,5 @@ Rails.application.routes.draw do
   get '/404', to: 'errors#not_found', as: :not_found
   get '/500', to: 'errors#internal_server_error', as: :internal_server_error
 
-  match '*path', to: 'errors#not_found', via: :all
+  match '*path', to: 'errors#internal_server_error', via: :all
 end
