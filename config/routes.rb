1234a55b1e12f4e1785972112362c73bb8ac13ab
EvAyano <g.ayanohisamoto@gmail.com>
2025-01-07 21:16:54 +0900

    added error feature for passoword reset view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: { 
+    registrations: 'users/registrations', 
+    passwords: 'users/passwords' 
+  }
 
   devise_scope :user do
     get 'users/edit_email', to: 'users/registrations#edit_email', as: 'edit_user_email'
@@ -17,9 +20,6 @@ Rails.application.routes.draw do
     patch 'users/update_avatar', to: 'users/registrations#update_avatar', as: 'update_user_avatar'
   end
   
-
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "home#index"
   
   resources :words, only: [:index, :show] do
@@ -41,11 +41,5 @@ Rails.application.routes.draw do
     end
   end
   
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
