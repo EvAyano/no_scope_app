1d2e997727d6a734ea43757881a86dd1eefafa1a
EvAyano <g.ayanohisamoto@gmail.com>
2025-01-29 02:22:04 +0900

    modified:   app/controllers/favorites_controller new file:   app/views/favorites/destroy_favorite.turbo_stream.erb

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -8,44 +8,44 @@ class FavoritesController < ApplicationController
   def create
     word = Word.find(params[:word_id])
     current_user.favorites.create(word: word)
-    
+
     respond_to do |format|
       format.turbo_stream do
-
-      youtube_id = if word.related_videos.present?
-                     word.related_videos
-                   else
-                     service = YoutubeService.new
-                     video   = service.search_video("counter strike #{word.term}")
-                     video&.id&.video_id
-                   end
-    
-      render "words/switch_favorite",locals: { word: word, youtube_video_id: youtube_id }
+        youtube_id = fetch_youtube_id_for(word)
+        render "words/switch_favorite",
+          locals: { word: word, youtube_video_id: youtube_id }
       end
+
+      format.html { redirect_to words_path }
     end
-    
-    format.html { redirect_to words_path }
   end
 
   def destroy
     favorite = current_user.favorites.find(params[:id])
     word = favorite.word
     favorite.destroy
+
+    youtube_id = fetch_youtube_id_for(word)
+
     @favorite_words = current_user.favorite_words.order(:term)
 
     respond_to do |format|
       format.turbo_stream do
-        youtube_id = if word.related_videos.present?
-                       word.related_videos
-                     else
-                       service = YoutubeService.new
-                       video   = service.search_video("counter strike #{word.term}")
-                       video&.id&.video_id
-                     end
-        render "favorites/update_list"
+        render "favorites/destroy_favorite", locals: { word: word, youtube_video_id: youtube_id }
       end
       format.html { redirect_to favorites_path }
     end
   end
-end
   
+  private
+  
+  def fetch_youtube_id_for(word)
+    if word.related_videos.present?
+      word.related_videos
+    else
+      service = YoutubeService.new
+      video   = service.search_video("counter strike #{word.term}")
+      video&.id&.video_id
+    end
+  end
+end
