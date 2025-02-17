be683ec9cb97629da4227dc880772b91dabcea1d
EvAyano <g.ayanohisamoto@gmail.com>
2025-02-17 22:23:26 +0900

    modified: home layouts modified: inquiry details

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,9 +5,9 @@ class HomeController < ApplicationController
     @recent_quizzes = Quiz.where(completed: true).order(start_time: :desc).limit(10)
 
     if @recent_quizzes.any?
-      @partial_to_render = "recent_quizzes_with_data"
+      @everyone_quiz_result = "recent_quizzes_with_data"
     else
-      @partial_to_render = "recent_quizzes_no_data"
+      @everyone_quiz_result = "recent_quizzes_no_data"
     end
   end
 
