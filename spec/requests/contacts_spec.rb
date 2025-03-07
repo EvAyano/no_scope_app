0e1a5111c15e631be3288ca131de3d2440b77969
EvAyano <g.ayanohisamoto@gmail.com>
2025-03-08 00:18:14 +0900

    modified:   spec/requests/contacts_spec.rb deleted:    spec/views/contacts/index.html.erb_spec.rb

--- spec/requests/contacts_spec.rb
+++ spec/requests/contacts_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Contacts", type: :request do
-  describe "GET /index" do
+RSpec.describe "Contacts", type: :request do  
+  describe "GET /new" do
     it "returns http success" do
-      get "/contacts/index"
+      get new_contact_path
       expect(response).to have_http_status(:success)
     end
   end
-
 end
