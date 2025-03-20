6cb7875a861976e102e4341144a8bef7a7f92d41
EvAyano <g.ayanohisamoto@gmail.com>
2025-03-20 23:58:37 +0900

    added shoulda-matchers to Gemfile added favorite model spec

---
+++ spec/models/favorite_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Favorite, type: :model do
+  let!(:user) { User.create!(email: 'test@noscope.com', password: 'password', password_confirmation: 'password', nickname: 'testuser') }
+
+  let(:word) do
+    Word.create!(
+      term: 'CSword', 
+      definition: 'CSword no imi',
+      explanation: 'Counter Strike no word',
+      example_en: 'Kauntaa Sutoraiku sentence',
+      example_jp: 'Counter Strike sentence',
+      pronunciation_jp: 'Shiesu',
+      pronunciation_en: 'CS'
+    )
+  end
+
+  describe 'お気に入りの一意性のバリデーション' do
+    before do
+      Favorite.create!(user: user, word: word)
+    end
+
+    it { is_expected.to validate_uniqueness_of(:user_id).scoped_to(:word_id) }
+  end
+
+  describe 'アソシエーション' do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:word) }
+  end
+end
