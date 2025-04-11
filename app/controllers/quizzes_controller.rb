0366f71e6aa821b19fe88834e076c577afcf40c0
EvAyano <g.ayanohisamoto@gmail.com>
2025-04-11 22:32:12 +0900

    modified quiz results page by eager loading words modified quiz history page by preloading quiz question count

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -39,7 +39,7 @@ class QuizzesController < ApplicationController
     page = params[:page] || 1
   
     #フィルターとページネーションを適用したクイズ履歴
-    paginated_quizzes = Quiz.paginated_quizzes_for(current_user, year: year, month: month, page: page)
+    paginated_quizzes = Quiz.paginated_quizzes_for(current_user, year: year, month: month, page: page).includes(:quiz_questions)
 
     #ビュー表示用データ
     @quizzes_log = Quiz.fetch_logs_from_relation(paginated_quizzes)
@@ -131,7 +131,7 @@ class QuizzesController < ApplicationController
 
   def load_results
     @quiz = Quiz.find(params[:id])
-    @questions = @quiz.quiz_questions
+    @questions = @quiz.quiz_questions.includes(:word)
     @quiz.calculate_and_save_score
     @show_start_time = @quiz.start_time.present? ? @quiz.start_time.in_time_zone('Tokyo').strftime('%Y-%m-%d %H:%M') : "不明"
   end
